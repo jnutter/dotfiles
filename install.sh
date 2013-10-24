07fed8b9ccb9fccfe9dd6ab2ef54393d7d9025d7
Jonathan Nutter <jnutter@gmail.com>
2013-10-23 17:04:48 -0700

    even more installs

--- install.sh
+++ install.sh
@@ -14,14 +14,21 @@ for src in $(find $PWD -maxdepth 1); do
 
   if [[
     $file != '.git' &&
-    $file != '.'    &&
-    $file != '..'   &&
-    $file != 'node_modules'   &&
-    $file != '.gitmodules'   &&
+    $file != '.' &&
+    $file != '..' &&
+    $file != 'node_modules' &&
+    $file != '.gitmodules' &&
+    $file != 'Preferences.sublime-settings' &&
     $file != $me
   ]]; then
     echo "Symlinking $file from $src"
     ln -Ffs $src ~/
+  fi
+  if [[ $file == 'Preferences.sublime-settings' ]]; then
+    if [ -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ]; then
+      rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+      ln -Ffs $src ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+    fi
   fi
 done
 
